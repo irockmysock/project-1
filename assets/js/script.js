df3fc408b5f83f37a63a2ab944ea940e255c5a30
irockmysock <benny_chin89@hotmail.com>
2019-07-07 17:35:53 +0800

    added stage 3 and disable on wrong click function. to fix stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ console.log("Shoot shoot game on!")
 //Global variables
 var stageOneArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 var stageTwoArray = [];
+var stageThreeArray = [];
 var boxesShot = [];
 var counter = 0;
 var CompleteStageOneTime = null;
@@ -14,18 +15,25 @@ var CompleteStageFourTime = null;
 ////////////////////
 // GAME FUNCTIONS //
 ///////////////////
+
+//shuffle game array to randomize numbers appearing on board
  var shuffleArray = function(array) {
         for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [array[i], array[j]] = [array[j], array[i]];
         }
-    }
+}
 
-    // shuffleArray(stageOneArray);
+//create random color to style shotbox
+var createRandomColor = function() {
+    let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    return(color);
+}
 
-    // console.log(stageOneArray);
-
-
+// set the shotbox color to random
+var setRandomColor = function() {
+    document.body.style.backgroundColor = createRandomColor();
+}
 
 // var fireOnBox   = function() {
 //         var bangSound = new Audio();
@@ -66,6 +74,52 @@ var readyGame = function() {
     displayTimer();
 }
 
+var checkWinStageOne = function(){
+    if (stageOneArray.length === boxesShot.length) {
+        // console.log("counter is: "+counter);
+        // var timing = (counter/100).toFixed(2)
+        // alert(`Your timing is ${CompleteStageOneTime} seconds`);
+        stopTimer();
+        if (parseFloat(document.getElementById("stage-1-time").innerText) === 0) {
+            document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
+        } else {
+                if (parseFloat(CompleteStageOneTime) < parseFloat(document.getElementById("stage-1-time").innerText)) {
+                document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
+                }
+        }
+    }
+}
+
+var checkWinStageTwo = function(){
+    if (stageTwoArray.length === boxesShot.length) {
+        // console.log("counter is: "+counter);
+        // var timing = (counter/100).toFixed(2)
+        // alert(`Your timing is ${CompleteStageOneTime} seconds`);
+        stopTimer();
+        if (parseFloat(document.getElementById("stage-2-time").innerText) === 0) {
+            document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
+        } else {
+                if (parseFloat(CompleteStageTwoTime) < parseFloat(document.getElementById("stage-2-time").innerText)) {
+                document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
+                }
+        }
+    }
+}
+
+var checkWinStageThree = function(){
+    if (stageThreeArray.length === boxesShot.length) {
+        stopTimer();
+        if (parseFloat(document.getElementById("stage-3-time").innerText) === 0) {
+            document.getElementById("stage-3-time").innerText = CompleteStageThreeTime;
+        } else {
+                if (parseFloat(CompleteStageThreeTime) < parseFloat(document.getElementById("stage-3-time").innerText)) {
+                document.getElementById("stage-3-time").innerText = CompleteStageThreeTime;
+                }
+        }
+    }
+}
+
+
 /////////////////////////////////////////////////////
 ////////////////Timer Functions//////////////////////
 var startTimer = "";
@@ -87,7 +141,7 @@ var clearTimer = function (time) {
     output.innerText = null;
 }
 
-//Stage 1 timer
+//Stage 1 timer to log stage completion timing
 var runTimer1= function(){
 
     //start timer
@@ -99,13 +153,27 @@ var runTimer1= function(){
         displayTimer(`${timing} seconds`);
     }, 10);
 }
-//Stage 2 timer
+
+//Stage 2 timer to log stage completion timing
 var runTimer2 = function(){
     //start timer
     startTimer = setInterval(function(){
         counter++;
         var timing = (counter/100).toFixed(2);
         CompleteStageTwoTime = timing
+        // console.log(counter);
+        displayTimer(`${timing} seconds`);
+    }, 10);
+}
+
+//Stage 3 timer to log stage completion timing
+var runTimer3= function(){
+
+    //start timer
+    startTimer = setInterval(function(){
+        counter++;
+        var timing = (counter/100).toFixed(2);
+        CompleteStageThreeTime = timing
         // console.log(counter);
         displayTimer(`${timing} seconds`);
     }, 10);
@@ -118,7 +186,7 @@ var runTimer2 = function(){
 ///////////////////       STAGE ONE        ///////////////////////////
 
 var stageOneStart = function() {
-
+    //clear timers and board and reset arrays
     readyGame();
     // clearBoard();
     // clearTimer();
@@ -145,67 +213,26 @@ var stageOneStart = function() {
         gameBoard.appendChild(shootBox);
     }
 
-
+    //  click function in game
     var fireOnBox   = function() {
         //add gunshot sound to click
         var bangSound = new Audio();
         bangSound.src = "assets/css/sounds/gunshot.mp3"
         bangSound.play();
-
+        //box disappears if shot in order
         if (parseInt(event.target.id) === boxesShot.length+1) {
             event.target.style.visibility = "hidden";
             boxesShot.push(event.target.id)
             console.log(boxesShot);
             checkWinStageOne();
         }
-        // else {
-        //     for ( i=0; i < stageOneArray.length; i++) {
-        //             var box = document.querySelectorAll(".shotBox")[i];
-        //             // box.style.backgroundColor = "black";
-        //             // document.querySelectorAll(".shotBox")[i].disabled = true;
-        //             box.style.visibility = "hidden";
-        //             var hangGame = setTimeout(function(box){
-        //                 box.style.visibility = "initial";
-        //             }, 3000);
-        //             // var hangGame = setTimeout(function(box){
-        //             //     box.style.backgroundColor = "initial";
-        //             // }, 3000);
-        //     }
-        //     return;
-        // }
     }
 
-    //function to hide boxes upon click
-    //push boxes that have been shot to boxesShot array
-    // var fireOnBox   = function() {
-    //     var bangSound = new Audio();
-    //     bangSound.src = "assets/css/sounds/gunshot.mp3"
-    //     bangSound.play();
-
-
-    //     if (parseInt(event.target.id) === boxesShot.length+1) {
-    //         event.target.style.visibility = "hidden";
-    //         boxesShot.push(event.target.id)
-    //         console.log(boxesShot);
-    //         checkWinStageOne();
-    //     } else {
-    //         for ( i=0; i < stageOneArray.length; i++) {
-    //                 var box = document.querySelectorAll(".shotBox")[i];
-    //                 box.style.backgroundColor = "black";
-    //                 document.querySelectorAll(".shotBox")[i].disabled = true;
-    //                 var hangGame = setTimeout(function(box){
-    //                     box.style.backgroundColor = "initial";
-    //                 }, 3000);
-    //         }
-    //         return;
-    //     }
-    // }
-
+    //add event listener for click
     for (i = 0; i < stageOneArray.length; i++) {
         var selectBox = document.querySelectorAll(".shotBox");
         selectBox[i].addEventListener('click',fireOnBox);
     }
-
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -217,9 +244,8 @@ var stageTwoStart = function() {
     backgroundSound.src = "assets/css/sounds/Surreal-Chase_Looping.mp3"
     // backgroundSound.play();
 
-    clearBoard();
-    clearTimer();
-    displayTimer();
+    //clear timers and board and reset arrays/
+    readyGame();
     runTimer2();
     stageTwoArray = [];
     shuffleArray(stageOneArray);
@@ -246,21 +272,27 @@ var stageTwoStart = function() {
         bangSound.src = "assets/css/sounds/gunshot.mp3"
         bangSound.play();
 
-
+        //box disappears if shot in order
         if (parseInt(event.target.id) === boxesShot.length+1) {
             event.target.style.visibility = "hidden";
             boxesShot.push(event.target.id)
             console.log(boxesShot);
             checkWinStageTwo();
         } else {
+            // box turns black but number still visible and click disabled for 2 seconds
             for ( i=0; i < stageTwoArray.length; i++) {
                     var box = document.querySelectorAll(".shotBox")[i];
                     box.style.backgroundColor = "black";
                     box.style.color = "grey";
-                    // document.querySelectorAll(".shotBox")[i].disabled = true;
-                    // var hangGame = setTimeout(function(box){
-                    //     box.style.backgroundColor = "initial";
-                    // }, 3000);
+                    box.removeEventListener('click', fireOnBox);
+
+                    var returnToNormal = setTimeout(function(box){
+                        for ( i=0; i < stageTwoArray.length; i++) {
+                            document.querySelectorAll(".shotBox")[i].style.backgroundColor = createRandomColor();
+                            document.querySelectorAll(".shotBox")[i].style.color = "white";
+                            document.querySelectorAll(".shotBox")[i].addEventListener('click',fireOnBox);
+                        }
+                    }, 2000);
             }
         }
     }
@@ -272,6 +304,80 @@ var stageTwoStart = function() {
 }
 
 
+///////////////////////////////////////////////////////////////////////
+///////////////////       STAGE THREE        ///////////////////////////
+
+var stageThreeStart = function() {
+
+    //clear timers and board and reset arrays
+    readyGame();
+    runTimer3();
+    stageThreeArray = [];
+    for (i = 1; i < 26; i++) {
+        stageThreeArray.push(i);
+    }
+    // console.log(stageThreeArray);
+    // console.log(stageThreeArray.length);
+
+    shuffleArray(stageThreeArray);
+
+    // console.log(stageThreeArray);
+
+    //add background in-game music
+    var backgroundSound = new Audio();
+    backgroundSound.src = "assets/css/sounds/Surreal-Chase_Looping.mp3"
+    // backgroundSound.play();
+
+    //create gameboard based on boardsize with random numbers
+    for (i = 0; i < stageThreeArray.length; i++) {
+        var shootBox = document.createElement("div");
+        shootBox.setAttribute("id", stageThreeArray[i]);
+        shootBox.setAttribute("class","shotBox");
+        shootBox.setAttribute("style","box-sizing: border-box; display: inline-block; height: 20%; width: 20%; border-radius: 15px;");
+        shootBox.style.backgroundColor = createRandomColor();
+        shootBox.innerText = stageThreeArray[i]
+        var gameBoard = document.querySelector(".game-board");
+        gameBoard.appendChild(shootBox);
+    }
+
+
+    var fireOnBox   = function() {
+        //add gunshot sound to click
+        var bangSound = new Audio();
+        bangSound.src = "assets/css/sounds/gunshot.mp3"
+        bangSound.play();
+        //box disappears if shot in order
+        if (parseInt(event.target.id) === boxesShot.length+1) {
+            event.target.style.visibility = "hidden";
+            boxesShot.push(event.target.id)
+            console.log(boxesShot);
+            checkWinStageThree();
+        } else {
+            // box turns black and click disabled for 2 seconds
+            for ( i=0; i < stageThreeArray.length; i++) {
+                    var box = document.querySelectorAll(".shotBox")[i];
+                    box.style.backgroundColor = "black";
+                    box.style.color = "black";
+                    box.removeEventListener('click', fireOnBox);
+
+                    var returnToNormal = setTimeout(function(box){
+                        for ( i=0; i < stageThreeArray.length; i++) {
+                            document.querySelectorAll(".shotBox")[i].style.backgroundColor = createRandomColor();
+                            document.querySelectorAll(".shotBox")[i].style.color = "white"
+                            document.querySelectorAll(".shotBox")[i].addEventListener('click',fireOnBox);
+                        }
+                    }, 2000);
+            }
+        }
+    }
+
+    //event listner for shot on box
+    for (i = 0; i < stageThreeArray.length; i++) {
+        var selectBox = document.querySelectorAll(".shotBox");
+        selectBox[i].addEventListener('click',fireOnBox);
+    }
+
+}
 
 
 
@@ -343,45 +449,48 @@ var stageTwoStart = function() {
 
 
 
-var checkWinStageOne = function(){
-    if (stageOneArray.length === boxesShot.length) {
-        // console.log("counter is: "+counter);
-        // var timing = (counter/100).toFixed(2)
-        // alert(`Your timing is ${CompleteStageOneTime} seconds`);
-        stopTimer();
-        if (parseFloat(document.getElementById("stage-1-time").innerText) === 0) {
-            document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
-        } else {
-                if (parseFloat(CompleteStageOneTime) < parseFloat(document.getElementById("stage-1-time").innerText)) {
-                document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
-                }
-        }
-    }
-}
+// var checkWinStageOne = function(){
+//     if (stageOneArray.length === boxesShot.length) {
+//         // console.log("counter is: "+counter);
+//         // var timing = (counter/100).toFixed(2)
+//         // alert(`Your timing is ${CompleteStageOneTime} seconds`);
+//         stopTimer();
+//         if (parseFloat(document.getElementById("stage-1-time").innerText) === 0) {
+//             document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
+//         } else {
+//                 if (parseFloat(CompleteStageOneTime) < parseFloat(document.getElementById("stage-1-time").innerText)) {
+//                 document.getElementById("stage-1-time").innerText = CompleteStageOneTime;
+//                 }
+//         }
+//     }
+// }
 
-var checkWinStageTwo = function(){
-    if (stageTwoArray.length === boxesShot.length) {
-        // console.log("counter is: "+counter);
-        // var timing = (counter/100).toFixed(2)
-        // alert(`Your timing is ${CompleteStageOneTime} seconds`);
-        stopTimer();
-        if (parseFloat(document.getElementById("stage-2-time").innerText) === 0) {
-            document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
-        } else {
-                if (parseFloat(CompleteStageTwoTime) < parseFloat(document.getElementById("stage-2-time").innerText)) {
-                document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
-                }
-        }
-    }
-}
+// var checkWinStageTwo = function(){
+//     if (stageTwoArray.length === boxesShot.length) {
+//         // console.log("counter is: "+counter);
+//         // var timing = (counter/100).toFixed(2)
+//         // alert(`Your timing is ${CompleteStageOneTime} seconds`);
+//         stopTimer();
+//         if (parseFloat(document.getElementById("stage-2-time").innerText) === 0) {
+//             document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
+//         } else {
+//                 if (parseFloat(CompleteStageTwoTime) < parseFloat(document.getElementById("stage-2-time").innerText)) {
+//                 document.getElementById("stage-2-time").innerText = CompleteStageTwoTime;
+//                 }
+//         }
+//     }
+// }
 
 // var checkWinStageThree = function(){
 //     if (stageThreeArray.length === boxesShot.length) {
-//         // console.log("counter is: "+counter);
-//         // var timing = (counter/100).toFixed(2)
-//         // alert(`Your timing is ${CompleteStageThreeTime} seconds`);
 //         stopTimer();
-//         document.getElementById("stage-3-time").innerText = CompleteStageThreeTime;
+//         if (parseFloat(document.getElementById("stage-3-time").innerText) === 0) {
+//             document.getElementById("stage-3-time").innerText = CompleteStageThreeTime;
+//         } else {
+//                 if (parseFloat(CompleteStageThreeTime) < parseFloat(document.getElementById("stage-3-time").innerText)) {
+//                 document.getElementById("stage-3-time").innerText = CompleteStageThreeTime;
+//                 }
+//         }
 //     }
 // }
 
@@ -399,6 +508,8 @@ var checkWinStageTwo = function(){
 
 document.querySelector("#stage1").addEventListener('click',stageOneStart);
 document.querySelector("#stage2").addEventListener('click',stageTwoStart);
+document.querySelector("#stage3").addEventListener('click',stageThreeStart);
+// document.querySelector("#stage4").addEventListener('click',stageFourStart);
 
 // document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -406,13 +517,13 @@ document.querySelector("#stage2").addEventListener('click',stageTwoStart);
 
 // });
 
-var createRandomColor = function() {
+// var createRandomColor = function() {
 
-    let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    return(color);
-}
+//     let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+//     return(color);
+// }
 
-var setRandomColor = function() {
+// var setRandomColor = function() {
 
-    document.body.style.backgroundColor = createRandomColor();
-}
+//     document.body.style.backgroundColor = createRandomColor();
+// }
